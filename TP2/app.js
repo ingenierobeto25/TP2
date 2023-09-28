d4545f66879668cb9b22529230623e06db14c95d
ingenierobeto25 <ingenierobeto25@gmail.com>
2023-09-27 21:53:04 -0400

    images et données sur HTML, il faut créer un container par personnage

--- TP2/app.js
+++ TP2/app.js
@@ -1,26 +1,31 @@
 //API à utiliser pour le travail: https://randomuser.me/api/?results=5
-let imagePersonnage = document.getElementById('image')
-console.log(imagePersonnage)
+
 fetch('https://randomuser.me/api/?results=5')
 .then(response=> response.json())
 .then(data=>{
-    let firstArray = Object.values(data)
-   // console.log(firstArray)
+    let firstArray = Object.values(data)   
     let secondArray = Object.values(firstArray[0])
-    console.log(secondArray) 
-    let output = '<h2>Listes de personnages</h2>' 
-    secondArray.forEach(function(element) {
-        
-        imagePersonnage.src = element.picture.large
-        console.log(imagePersonnage)
-        output += element.name.title +' '+ element.name.first + ' ' + element.name.last + '<br/> '
-        output += element.email + '<br/>'
-        
-    })
-   // document.getElementById('image').innerHTML = image   
-    document.getElementById('container').innerHTML = output
-    
-    }) 
+    let imagePersonnage    
+    let output = ['<h2>Listes de personnages</h2>']   
 
+    secondArray.forEach(function(element) {        
+        imagePersonnage = element.picture.large         
+        names = element.name.title +' '+ element.name.first + ' ' + element.name.last + '<br/> '
+        emails = element.email + '<br/>'
+        showImage(imagePersonnage)
+        printInfo(names, emails)              
+    })
+
+    function printInfo(names, emails){       
+        output += names + emails
+        document.getElementById('container').innerHTML = output    
+   }
+
+   function showImage(imagePersonnage){
+        const showImg = document.createElement('img')
+        showImg.src=imagePersonnage
+        document.body.appendChild(showImg)
+   }
+})
 .catch(error => console.log(error));
 
